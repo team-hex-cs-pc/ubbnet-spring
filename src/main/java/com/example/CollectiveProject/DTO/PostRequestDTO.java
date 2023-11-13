ddba639513e530cea061d168c513be8a61fe35e2
BogdanModolea <bogdanmodolea30@gmail.com>
2023-11-13 16:47:38 +0200

    Modified Controllers & APIs
    -> Tested in postman (have to figure out something about some ids in the services)
    -> Removed logic in controllers
    -> Removed ids from URI
    -> Some bug fixes

--- src/main/java/com/example/CollectiveProject/DTO/PostRequestDTO.java
+++ src/main/java/com/example/CollectiveProject/DTO/PostRequestDTO.java
@@ -4,9 +4,11 @@ import lombok.Data;
 
 @Data
 public class PostRequestDTO {
+    private String postReference;
     private String title;
     private String content;
     private String category;
+    private int likes;
     private String publicationDate;
     private String username;
 }
