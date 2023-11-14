fbcc63a01342f1211120cc24c31d5e108bdff6f3
amihai <alex.mihai.email@gmail.com>
2023-11-14 23:22:43 +0200

    Add dates in mappers

--- src/main/java/com/example/CollectiveProject/Service/UserService.java
+++ src/main/java/com/example/CollectiveProject/Service/UserService.java
@@ -10,15 +10,14 @@ import com.example.CollectiveProject.Mapper.PostMapper;
 import com.example.CollectiveProject.Mapper.UserMapper;
 import com.example.CollectiveProject.Repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.example.CollectiveProject.Utilities.Constants;
 
+import com.example.CollectiveProject.Utilities.Constants;
 import java.util.List;
 import java.util.stream.Collectors;
 
